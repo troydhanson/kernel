1c2d5d25b8ed2a33e14abca78d444e54383d2069
Troy D. Hanson <tdh@tkhanson.net>
2015-02-22 17:00:04 -0500

    read

--- 05.chardev_ops/README.md
+++ 05.chardev_ops/README.md
@@ -1,9 +1,25 @@
 The "chardev_ops" module adds ops to our device driver.
 The ops are open, release (close), and read.
 
-  % sudo insmod chardev.ko 
+  % sudo insmod chardev_ops.ko 
 
-Determine the major.
-Make the device node.
-Read from it. 
-Delete the device node.
+Determine the major:
+
+% cat /proc/devices | grep chardev_ops
+250 chardev_ops
+
+Make the device node:
+
+  % sudo mknod dev c 250 0
+
+Read from it:
+
+  % 2281  dd if=dev of=out bs=10 count=1
+  % 2284  dd if=dev of=out bs=100 count=1
+  % 2289  dd if=dev of=out bs=101 count=1
+
+This driver returns 100 bytes max. The last dd only reads 100 bytes.
+
+Delete the device node:
+
+  % rm dev
--- 05.chardev_ops/chardev_ops.c
+++ 05.chardev_ops/chardev_ops.c
@@ -1,6 +1,7 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
+#include <asm/uaccess.h>
 #include <linux/cdev.h>
 #include <linux/fs.h>
 
@@ -11,41 +12,57 @@ MODULE_LICENSE("Dual BSD/GPL");
 
 #define NUM_MINORS 1
 
-/* a global in the driver to keep state */
+/* a global to keep state. must be thread safe. */
 struct chardev_t {
   dev_t dev;        /* has major and minor bits */
   struct cdev cdev; /* has our ops, owner, etc */
 } c;
 
-static int _open(struct inode *inode, struct file *filp) {
+int _open(struct inode *inode, struct file *f) {
   void *per_fd_state = NULL;
   /* we can store away some per fd state here. */
-  filp->private_data = per_fd_state;
+  f->private_data = per_fd_state;
   /* we can access the rw and blocking mode */
-  printk(KERN_DEBUG "open for %c%c\n", ((filp->f_mode & FMODE_READ) ? 'r':'-'),
-                                      ((filp->f_mode & FMODE_WRITE) ? 'w':'-'));
+  printk(KERN_DEBUG "open for %c%c\n", ((f->f_mode & FMODE_READ)  ? 'r':'-'),
+                                       ((f->f_mode & FMODE_WRITE) ? 'w':'-'));
   return 0;
 }
 
 /* kernel calls release when fd usage count reaches 0. e.g. if a process opens
  * a device, forks, fd usage count is now two; when both close, its released. */
-static int _release(struct inode *inode, struct file *filp) {
+int _release(struct inode *inode, struct file *f) {
   return 0;
 }
 
+/* read data into the userspace buf. some notes:
+   1. buf is annotated with a leading __user to help us remember its userspace
+   2. on real device a 'read' may block. 
+   3. a driver can implement both blocking and non-blocking mode if desired.
+   4. copy_to_user can sleep (e.g. if userspace buffer needs to be paged in).
+   this example returns a zeroed buffer.
+*/
+char device_buf[100];
+ssize_t _read(struct file *f, char __user *buf, size_t count, loff_t *offp) {
+  ssize_t c = (count < sizeof(device_buf)) ? count : sizeof(device_buf);
+  memset(device_buf,0,c);
+  if (copy_to_user(buf, device_buf, c)) return -EFAULT;
+  *offp += c;
+  return c;
+}
+
 struct file_operations ops = {
-  .read = NULL,
+  .read = _read,
   .write = NULL,
   .unlocked_ioctl = NULL,
   .open = _open,
-  .release = NULL
+  .release = _release
 };
 
 int __init chardev_init(void) {
   int rc;
 
   /* ask for a dynamic major */
-  rc = alloc_chrdev_region(&c.dev, 0, NUM_MINORS, "chardev");
+  rc = alloc_chrdev_region(&c.dev, 0, NUM_MINORS, "chardev_ops");
   if (rc) {
     printk(KERN_WARNING "alloc_chrdev_region: failed to get major\n");
     goto done;
